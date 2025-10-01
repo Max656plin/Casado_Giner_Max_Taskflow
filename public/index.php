4d68e723981c3bce9927f45daafdb985d91ed2f4
Max656plin <maximocasadoginer@gmail.com>
2025-10-01 12:23:32 +0200

    New changes

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
+<?php
+    define("SITE_NAME", "TaskFlow");
+    $pageTitle = SITE_NAME . " - Página de Inicio";
 
+    $userName = "Máximo Casado Giner"; // Tipo String
+    $userAge = 21;             // Tipo Integer
+    $isPremiumUser = false;     // Tipo Boolean
+
+    
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle; ?></title>
+</head>
+<body>
+    <h1>Bienvenido a <?php echo SITE_NAME; ?></h1>
+
+    <main>
+        <h2>Perfil del Usuario</h2>
+        <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
+        <p><strong>Edad:</strong> <?php echo $userAge; ?> años</p>
+        <p><strong>Estado de la cuenta:</strong> Usuario <?php echo $isPremiumUser ? "Premium" : "Estándar"; ?></p>
+    </main>
+</body>
+</html>
