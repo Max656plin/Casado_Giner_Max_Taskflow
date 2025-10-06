ba403879b886362e7330ce63ec78567a3533c70d
Max656plin <maximocasadoginer@gmail.com>
2025-10-06 12:08:45 +0200

    Add problemas.php with variable declarations and outputs

---
+++ public/problemas.php
@@ -0,0 +1,30 @@
+<?php
+    /*
+    * Ejercicio 1: Declarar variables de diferentes tipos y mostrarlas en pantalla.
+    */
+    $nombre = "Hideo";
+    $apellidos = "Kojima";
+    $edad = 62;
+    $altura = 1.73;
+    $estudiante = false;
+
+    echo "Nombre: $nombre ";
+    echo " Apellidos: $apellidos ";
+    echo " Edad: $edad ";
+    echo " Altura: $altura metros ";
+    echo " ¿Es estudiante?: " . ($estudiante ? "Sí" : "No");
+?>
+
+<?php
+    $producto = "Portátil";
+    $precio = 499.99;
+    $stock = 25;
+    $oferta = true;
+
+    echo "Nombre: $producto ";
+    echo " Precio: $precio €";
+    echo " stock: $stock ";
+    echo " : $altura metros ";
+    echo " ¿Es estudiante?: " . ($estudiante ? "Sí" : "No");
+
+?>
