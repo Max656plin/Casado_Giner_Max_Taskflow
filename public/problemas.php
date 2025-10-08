ec406b54e009a90b19b724466128b3bbb8ea0eb1
Max656plin <maximocasadoginer@gmail.com>
2025-10-08 13:03:09 +0200

    Resuelve la batería de problemas de la UD2

--- public/problemas.php
+++ public/problemas.php
@@ -53,6 +53,7 @@
     echo "División: " . ($num1 / $num2);
 ?>
 
+
 <?php
     /*
     * Ejercicio 2.2: Variables para gestionar el stock de un producto en una tienda online.
@@ -83,10 +84,237 @@
 ?>
 
 <?php
+    /*
+    * Ejercicio 2.4: Comparar dos números simples y mostrar el mayor
+    */
     $edad = 20;
     $edadMinima = 18;
 
-    var_dump($edad > $edadMinima);  // true
-    var_dump($edad < $edadMinima);  // false    
+    var_dump($edad >= $edadMinima);  // true
+    
+    $contraseñaGuardada = "123";
+    $contaseñaUsuario = "123";
+
+    var_dump($contraseñaGuardada === $contaseñaUsuario);  // true   
 ?>
 
+<?php
+    /*
+    *Ejercicio 2.5: comprobar si un número es par o impar
+    */ 
+    $numero = 11;
+
+    if ($numero % 2 == 0)
+        {
+            echo "El número $numero es par.";
+        } 
+        else 
+        {
+            echo "El número $numero es impar.";
+        }
+?>
+
+<?php
+       /*
+        * Ejercicio 3.1: Comprobar si una persona pasa o no basandose en su edad 
+        */
+    $edad = 21;
+    if ($edad >= 18) 
+        {
+            echo "Puedes pasar";
+        } 
+        else 
+        {
+            echo "No puedes pasar";
+        }
+?>
+
+<?php
+    /*
+    * Ejercicio 3.2: Calcular la calificación de un estudiante basandose en su nota numérica
+    */
+    $nota=5;
+
+    if ($nota >= 9) 
+        {
+            echo "Sobresaliente";
+        } 
+        elseif ($nota >= 7 && $nota < 8.9) 
+        {
+            echo "Notable";
+        } 
+        elseif ($nota >= 5 && $nota < 6.9) 
+        {
+            echo "Aprobado";
+        }
+        else 
+        {
+            echo "Suspenso";
+        }
+?>
+
+<?php
+    /*
+    * Ejercicio 3.3: Mostrar el menú del día basandose en el día de la semana
+    */
+
+    $diaSemana = "lunes";
+
+    switch ($diaSemana)
+     {
+        case "lunes":
+            echo "Lentejas";
+            break;
+
+        case "miercoles":
+            echo "Pealla";
+            break;
+
+        case "viernes":
+            echo "Pescado al horno";
+            break;  
+
+        default:
+            echo "Hoy no hay menú especial";
+            break;
+     }
+?>
+
+<?php
+    /*
+    * Ejercicio 3.4: Verficación de acceso en un bucle if
+    */
+    
+    $rolUsuario = "admin";
+    $usuarioActivo = true;
+
+    if ($rolUsuario === "admin" && $usuarioActivo) 
+        {
+            echo "Acceso concedido";
+        } 
+        else 
+        {
+            echo "Acceso denegado";
+        }
+?>
+
+<?php
+    /*
+    * Ejercicio 4.1: Mostrar los números del 1 al 10 utilizando un bucle for
+    */
+ $numeroMultiplicar = 7;
+    for ($i = 1; $i <= 10; $i++) 
+        {
+            $resultado = $numeroMultiplicar * $i;
+            echo "$numeroMultiplicar x $i = $resultado\n";
+        }
+?>
+
+<?php 
+    /*
+    * Ejercicio 4.2: Mostrar los números del 10 al 1 utilizando un bucle while
+    */
+
+  $a = 10;
+
+  while ($a >= 0) 
+      {
+          echo $a . "\n";
+          $a--;
+      }
+      echo "¡Feliz Año Nuevo!";
+?>
+
+<?php
+    /*
+    * Ejercicio 4.3: Iterar sobre un array de nombres y mostrar un  mensaje de saludo para cada nombre
+    */
+    $alumnos = ["Ana", "Luis", "Pedro", "Marta"];
+
+    foreach ($alumnos as $alumno) 
+    {
+        echo "Hola, $alumno!\n";
+    }
+?>
+
+<?php
+    /*
+    * Ejercicio 4.4: Iterar sobre un array asociativo de productos y mostrar su nombre y precio
+    */
+$productos = [
+    ["nombre" => "Portátil", "precio" => 50],
+    ["nombre" => "Ratón", "precio" => 25],
+    ["nombre" => "Teclado", "precio" => 200]
+];
+
+foreach ($productos as $productos) 
+{
+    echo "Producto: " . $productos["nombre"] . " - Precio: " . $productos["precio"] . " €\n";
+}
+?>
+
+<?php
+/*
+* Ejercicio 5.1: Iterar sobre un array asociativo y mostrar sus claves y valores
+*/  
+
+$usuario = ["nombre" => "Tu Nombre","edad" => 30, "email" => "tu.email@ejemplo.com", "esEstudiante" => true];
+
+foreach ($usuario as $clave => $valor) 
+{
+    echo "$clave: $valor\n";
+}
+?>
+
+<?php
+    /*
+    * Ejercicio 5.2: Iterar sobre un array de notas y mostrar si cada nota es aprobada o suspendida
+    */
+
+    $notas = [8.5 , 4.2 , 7.0 , 9.8 , 5.5 , 3.9];
+    foreach ($notas as $notas)
+        if( $notas >= 5)
+            {
+                echo "Aprobado: $notas\n";
+            }
+            else
+            {
+                echo "Suspenso: $notas\n";
+            }
+?>
+
+<?php
+/*
+* Ejercicio 5.3: Iterar sobre un array multidimensional y mostrar sus elementos
+*/
+
+$listaCompra = [
+    "Frutas" => ["Manzanas", "Plátanos", "Naranjas"],
+    "Lácteos" => ["Leche", "Queso"],
+    "Limpieza" => ["Detergente", "Lejía"]
+];
+
+
+foreach ($listaCompra as $categoria => $productos) {
+    echo $categoria;
+    foreach ($productos as $producto) {
+        echo $producto;
+    }
+}
+?>
+
+<?php
+    $productos = [
+    ["nombre" => "Portátil", "precio" => 50],
+    ["nombre" => "Ratón", "precio" => 25],
+    ["nombre" => "Teclado", "precio" => 200]
+    ];
+    $total = 0;
+
+    foreach ($productos as $producto)
+        {
+            $total += $producto["precio"];
+        }
+        echo "El coste total del carrito de la compra es: $total €";
+        echo "El coste total del carrito de la compra es: " . $total . "€";
+?>
