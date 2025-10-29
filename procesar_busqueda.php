19e435969b53ad669f10911a89d9c120be9d922c
Máximo Casado Giner <maximocasadoginer@gmail.com>
2025-10-29 09:07:10 +0100

    Delete procesar_busqueda.php
    Este archivo era de prueba

--- procesar_busqueda.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['termino_busqueda'])) {
-    $termino = htmlspecialchars($_GET['termino_busqueda']);
-} else {
-    $termino = "Ningún término introducido";
-}
-echo "Has buscado: " . $termino;
-?>
