47eb66b708693ae3a1f920bdb6af821819a038c6
Máximo Casado Giner <maximocasadoginer@gmail.com>
2025-11-13 08:29:23 +0100

    AuthController Modificado

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,26 +1,25 @@
 <?php
-session_start();
-
-function handleLogin($email, $password, $usuarios_bbdd)
-{
-    if (isset($usuarios_bbdd[$email]) && $usuarios_bbdd[$email]['password'] === $password) 
-        {
-            $_SESSION['user_id'] = $usuarios_bbdd[$email]['id'];
-            $_SESSION['user_name'] = $usuarios_bbdd[$email]['nombre'];
+// app/controllers/AuthController.php
+// Antes: session_start();
+    session_start();
+    
+function handleLogin($email, $password, $usuarios_bbdd) {
+    
+    if (isset($usuarios_bbdd[$email]) && $usuarios_bbdd[$email]['password']
+=== $password) {
+        $_SESSION['user_id'] = $usuarios_bbdd[$email]['id'];
+        $_SESSION['user_nombre'] = $usuarios_bbdd[$email]['nombre'];
         return true;
-        }
+    }
     return false;
 }
 
-function checkAuth()
-{
-    return isset($_SESSION['user_id']);
-}
-
-function handleLogout()
-{
+function handleLogout() {
     session_destroy();
     header('Location: index.php?accion=login');
-    exit();
+    exit;
+}
+function checkAuth() {
+    return isset($_SESSION['user_id']);
 }
 ?>
