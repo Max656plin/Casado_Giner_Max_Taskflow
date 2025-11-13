add29fb921bde9239c1bff97c054b56506ea0469
Máximo Casado Giner <maximocasadoginer@gmail.com>
2025-11-13 08:30:23 +0100

    data.php mod

--- app/data.php
+++ app/data.php
@@ -2,6 +2,6 @@
 $usuarios_bbdd = [
     'usuario1@taskflow.com' => ['id' => 1,'nombre' => 'Maxim Masin','password' => 'pass123']
     ,
-    'usuario2@taskflow.com' => ['id' => 2,'nombre' => 'Vero Alumna','password' => 'admin']
+    'usuario2@taskflow.com' => ['id' => 2,'nombre' => 'Carla Alumna','password' => 'admin']
 ];
 ?>
